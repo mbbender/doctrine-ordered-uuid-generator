5de9b801ad6e25100efa0032b69f875bc564ad9d
Michael Bender <michaelbenbender@gmail.com>
2015-05-01 17:43:58 -0400

    Update path to match Doctrine

--- src/DBAL/Types/OrderedGuidType.php
+++ src/DBAL/Types/OrderedGuidType.php
@@ -23,6 +23,22 @@ class OrderedGuidType extends GuidType
     /**
      * {@inheritdoc}
      */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return null;
+            //TODO: Throw exception? Never should have a null value here I don't think
+        }
+
+        // TODO: Throw exception if $value is not a resource. Not sure how you can not have a resource.
+        $value = (is_resource($value)) ? stream_get_contents($value) : $value;
+
+        return bin2hex($value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return self::ORDEREDGUID;
